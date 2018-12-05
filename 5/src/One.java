cc76f024de45cbb46d924568084ec1bba8aa8aac
KawaiiJess <KawaiiJessJ@Gmail.com>
2018-12-04 21:32:15 -0800

    Finished part 1

---
+++ 5/src/One.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class One
+{
+    public static void main(String[] args) throws IOException
+    {
+        Scanner in = getScanner(args);
+        String test = in.next();
+        //String test = "dabAcCaBADcCcaDaA";
+        boolean removed = true;
+
+        while (removed)
+        {
+            for (int i = 0; i < test.length() - 1; i++)
+            {
+                if ((test.charAt(i) + 32) == test.charAt(i + 1) || (test.charAt(i) - 32) == test.charAt(i + 1))
+                {
+                    test = test.substring(0, i) + test.substring(i + 2, test.length());
+                    removed = true;
+                    break;
+                }
+                removed = false;
+            }
+        }
+        System.out.println(test);
+        System.out.println(test.length());
+    }
+
+    private static Scanner getScanner(String[] args) throws IOException
+    {
+        File file;
+
+        if (args.length == 0)
+        {
+            file = new File("./input.txt");
+        }
+        else
+        {
+            file = new File("./" + args[0]);
+        }
+
+        return new Scanner(file);
+    }
+}
