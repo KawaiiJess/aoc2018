5e94490339743198880a9e472b52ebd47b91368b
KawaiiJess <KawaiiJessJ@Gmail.com>
2018-12-11 16:02:13 -0800

    Added a int[] for part 2 with a method to set.

--- 11/src/Cell.java
+++ 11/src/Cell.java
@@ -2,6 +2,17 @@ class Cell
 {
     int value;
     int total;
+    int[] totals;
+
+    Cell()
+    {
+        totals = new int[301];
+    }
+
+    public void setTotals(int index, int pl)
+    {
+        totals[index] += pl;
+    }
 
     @Override
     public String toString()
