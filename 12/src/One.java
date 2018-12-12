71949489f15c62e78cab0569bc66e3c9ef821dbf
KawaiiJess <KawaiiJessJ@Gmail.com>
2018-12-12 00:03:39 -0800

    Wrong answer to part 1. Too low T_T

--- 12/src/One.java
+++ 12/src/One.java
@@ -1,12 +1,7 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class One
 {
     public static void main(String[] args)
     {
-        /*
         String rules = "####. => #\n" +
                 "..#.. => .\n" +
                 "#.#.. => .\n" +
@@ -38,8 +33,8 @@ public class One
                 "#...# => #\n" +
                 "..##. => .\n" +
                 "##### => #\n" +
-                "..#.# => #";*/
-
+                "..#.# => #";
+/*
         String rules = "...## => #\n" +
                 "..#.. => #\n" +
                 ".#... => #\n" +
@@ -53,28 +48,12 @@ public class One
                 "##.## => #\n" +
                 "###.. => #\n" +
                 "###.# => #\n" +
-                "####. => #";
+                "####. => #";*/
 
-        String[] temp = rules.split("\n");
-        List<String> potRules = new ArrayList<>();
-        List<String> notRules = new ArrayList<>();
-        for (String t : temp)
-        {
-            String[] parts = t.split(" => ");
-            if (parts[1].equals("#"))
-            {
-                potRules.add(parts[0]);
-            }
-            else
-            {
-                notRules.add(parts[0]);
-            }
-        }
-
-        //System.out.println(potRules.toString());
+        String[] potRules = rules.split("\n");
         String forty = "........................................";
-        String init = "#..#.#..##......###...###";
-        //String init = "#.#.#....##...##...##...#.##.#.###...#.##...#....#.#...#.##.........#.#...#..##.#.....#..#.###";
+        //String init = "#..#.#..##......###...###";
+        String init = "#.#.#....##...##...##...#.##.#.###...#.##...#....#.#...#.##.........#.#...#..##.#.....#..#.###";
         String ext = forty + init + forty;
 
         int[] result = new int[ext.length()];
@@ -84,15 +63,32 @@ public class One
         {
             for (String r : potRules)
             {
-                if (ext.contains(r))
+                String[] parts = r.split(" => ");
+                String rule = parts[0];
+                String value = parts[1];
+                if (ext.contains(rule))
                 {
-                    int first = ext.indexOf(r) + 2;
-                    result[first] = 1;
-
-                    while (first <= ext.lastIndexOf(r))
+                    int first = ext.indexOf(rule) + 2;
+                    if (value.equals("#"))
                     {
-                        first = ext.indexOf(r, first);
-                        result[first + 2] = 1;
+                        result[first] = 1;
+                    }
+                    else
+                    {
+                        result[first] = 0;
+                    }
+
+                    while (first <= ext.lastIndexOf(rule))
+                    {
+                        first = ext.indexOf(rule, first);
+                        if (value.equals("#"))
+                        {
+                            result[first + 2] = 1;
+                        }
+                        else
+                        {
+                            result[first + 2] = 0;
+                        }
                         first++;
                     }
                 }
@@ -111,7 +107,6 @@ public class One
                 }
             }
             ext = iter;
-            //System.out.println(Arrays.toString(result));
             if (i != 19)
             {
                 result = new int[ext.length()];
@@ -119,18 +114,16 @@ public class One
 
             System.out.printf("%2d: %s\n", (i + 1), ext);
         }
-        System.out.println(Arrays.toString(result));
 
         int total = 0;
         for (int i = 0; i < result.length; i++)
         {
             if (result[i] == 1)
             {
-                System.out.print(i - 40 + " ");
                 total += i - 40;
             }
         }
-        System.out.println("\n                                            0         1         2         3         4         5         6         7         8         9");
+        System.out.println("                                            0         1         2         3         4         5         6         7         8         9");
         System.out.println(total);
 
         //1688 IS WRONG, TOO LOW
