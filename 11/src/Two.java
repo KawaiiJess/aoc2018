4b8298bac0354b24672586da1ac54927af239b82
KawaiiJess <KawaiiJessJ@Gmail.com>
2018-12-11 18:51:38 -0800

    Finished part 2

--- 11/src/Two.java
+++ 11/src/Two.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class Two
 {
     public static void main(String[] args)
@@ -19,12 +17,11 @@ public class Two
             }
         }
 
-        //WALK GRID TO PLACE VALUES AND TOTALS
+        //WALK GRID ONCE TO PLACE VALUES
         for (int y = 1; y < grid[0].length; y++)
         {
             for (int x = 1; x < grid[0].length; x++)
             {
-                //STORE VALUE AT EACH
                 int rack = x + 10;
                 int pl = ((rack * y) + serial) * rack;
 
@@ -41,21 +38,21 @@ public class Two
             }
         }
 
-        int gs = 300;
-        //CALCULATE TOTALS
+        //WALK GRID AGAIN TO CALCULATE SUBGRIDS (Max sub-grid size of 50)
+        int maxGrid = 50;
         for (int y = 1; y < grid[0].length; y++)
         {
             for (int x = 1; x < grid[0].length; x++)
             {
-                for (int yd = y; yd < y + gs && yd < grid[y].length; yd++)
+                for (int yd = y; yd < y + maxGrid && yd < grid[y].length; yd++)
                 {
-                    for (int xd = x; xd < x + gs && xd < grid[x].length; xd++)
+                    for (int xd = x; xd < x + maxGrid && xd < grid[x].length; xd++)
                     {
+                        //Cool ass math to sum the variable size grids into a 1D array
                         int maxx = xd - x + 1;
                         int maxy = yd - y + 1;
                         int max = Math.max(maxx, maxy);
-                        grid[x][y].total += grid[xd][yd].value;
-                        for (int i = max; i < gs + 1; i++)
+                        for (int i = max; i < maxGrid + 1; i++)
                         {
                             grid[x][y].setTotals(i, grid[xd][yd].value);
                         }
@@ -64,12 +61,11 @@ public class Two
             }
         }
 
+        //FIND THE AREA WITH THE HIGHEST POWER LEVEL! (It's over 9000!)
         int x1 = 0;
         int y1 = 0;
         int index = 0;
         int high = 0;
-
-        //WALK GRID AGAIN
         for (int y = 1; y < grid[0].length; y++)
         {
             for (int x = 1; x < grid[0].length; x++)
@@ -77,7 +73,8 @@ public class Two
                 int[] temp = grid[x][y].totals;
                 for (int i = 1; i < temp.length; i++)
                 {
-                    if (temp[i] > high)
+                    //This was an edge case to exclude non-square answers bottom right. 290,284,15 IS WRONG
+                    if (temp[i] > high && i + y < grid[0].length && i + x < grid[0].length)
                     {
                         high = temp[i];
                         index = i;
@@ -88,38 +85,23 @@ public class Two
             }
         }
 
-        printSmallGrid(grid, x1, y1);
+        printSmallGrid(grid, x1, y1, index);
         System.out.println(x1 + "," + y1 + "," + index);
         System.out.println("HIGHEST: " + high);
-        System.out.println(Arrays.toString(grid[x1][y1].totals));
-        //290,284,15 IS WRONG
+        //System.out.println(Arrays.toString(grid[x1][y1].totals));
 
         long stop = System.currentTimeMillis();
         System.out.println("Took " + (stop - start) + " milliseconds");
     }
 
-    private static void printSmallGrid(Cell[][] grid, int x, int y)
+    private static void printSmallGrid(Cell[][] grid, int x, int y, int index)
     {
-        for (int yd = y - 1; yd < y + 4; yd++)
+        for (int yd = y - 1; yd < y + index + 1; yd++)
         {
-            for (int xd = x - 1; xd < x + 4; xd++)
+            for (int xd = x - 1; xd < x + index + 1; xd++)
             {
                 int val = grid[xd][yd].value;
                 System.out.printf("%3d", val);
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-
-    private static void printGridValue(Cell[][] grid)
-    {
-        for (int y = 1; y < grid.length; y++)
-        {
-            for (int x = 1; x < grid.length; x++)
-            {
-                int val = grid[x][y].value;
-                System.out.printf("%5d", val);
             }
             System.out.println();
         }
